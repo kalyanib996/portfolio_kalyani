b5d45b54af6db82b25b4c1962ee7effc911bcf1f
kalyanib996 <#github2305>
2024-05-31 18:14:10 -0600

    Updates

---
+++ precache-manifest.672cb63efa6008da7e4ad6865d46ba69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71ad8ad6593ab0811d434351d054c570",
+    "url": "/portfolio_kalyani/index.html"
+  },
+  {
+    "revision": "6920d7607bbf9d2fa3da",
+    "url": "/portfolio_kalyani/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "c40a847363265ef6e40f",
+    "url": "/portfolio_kalyani/static/css/main.d59df61f.chunk.css"
+  },
+  {
+    "revision": "6920d7607bbf9d2fa3da",
+    "url": "/portfolio_kalyani/static/js/2.3d1737ab.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio_kalyani/static/js/2.3d1737ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c40a847363265ef6e40f",
+    "url": "/portfolio_kalyani/static/js/main.21f96c61.chunk.js"
+  },
+  {
+    "revision": "2f3da6d229e4db74dd96",
+    "url": "/portfolio_kalyani/static/js/runtime-main.e826cd49.js"
+  }
+]);
